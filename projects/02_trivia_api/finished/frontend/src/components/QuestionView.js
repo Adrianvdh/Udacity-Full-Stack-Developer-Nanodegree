757ee5c41fef3bfda151a6ce2ec1d2b7e9dfe480
Adrian van den Houten <adrianvdh@gmail.com>
2021-02-15 20:56:35 +0200

    Trivia Project: Update FE TODOs.

--- projects/02_trivia_api/finished/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/finished/frontend/src/components/QuestionView.js
@@ -110,7 +110,7 @@ class QuestionView extends Component {
     if(action === 'DELETE') {
       if(window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
-          url: `/questions/${id}`, //TODO: update request URL
+          url: `/questions/${id}`, //TODO DONE: update request URL
           type: "DELETE",
           success: (result) => {
             this.getQuestions();
